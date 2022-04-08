8bda45fc1cfbabab87b0f36c5894e9f628677036
antonjonn <jonninenanton@gmail.com>
2022-04-08 10:37:40 +0300

    all kinds of styuff

--- PalvelinOhjelmointi/src/main/java/CandidateAnswer.java
+++ PalvelinOhjelmointi/src/main/java/CandidateAnswer.java
@@ -1,4 +1,53 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
-public class CandidateAnswer {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import dao.Dao;
+import data.Candidate;
 
-}
+/**
+ * Servlet implementation class CandidateAnswer
+ */
+@WebServlet("/CandidateAnswer")
+
+public class CandidateAnswer extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	private Dao dao=null;
+
+	@Override
+	public void init() {
+		dao=new Dao("jdbc:mysql://localhost:3306/vaalikone", "root", "salasana");
+	}
+	/**
+     * @see HttpServlet#HttpServlet()
+     */
+	
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id=Integer.parseInt(request.getParameter("id"));
+		//ArrayList<String> yeppers = new ArrayList<String>();
+		Candidate candi = null;
+		String ye = null;
+		ArrayList<String> f = new ArrayList<String>();
+		if (dao.getConnection()) {
+			candi = dao.readCandidate(id);
+			f.add(candi.getEtunimi());
+			//candi.getEtunimi() + candi.getSukunimi() + candi.getAmmatti() + candi.getIka() + candi.getKotipaikkakunta() + candi.getMiksi_eduskuntaan() + candi.getMita_edistaa() + candi.getPuolue();
+		}
+		else {
+			System.out.println("No connection to database");
+		}
+	
+		request.setAttribute("candidateanswer", f);
+		RequestDispatcher rd=request.getRequestDispatcher("/jsp/candidateanswer.jsp");
+		rd.forward(request, response);
+	}}
--- PalvelinOhjelmointi/src/main/java/Update.java
+++ PalvelinOhjelmointi/src/main/java/Update.java
@@ -1,6 +1,16 @@
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import dao.Dao;
-import data.Question
+import data.Question;
 
 @WebServlet("/updatequestion")
 
@@ -8,20 +18,19 @@ public class Update extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Dao dao = null;
 
-}
 
 @Override
 public void init() {
 	dao=new Dao("jdbc:mysql://localhost:3306/vaalikone", "root", "salasana");
 }
 
-	if(request.getParameter("Action").equals("Edit"))
+	//if(request.getParameter("Action").equals("Edit"))
 
-	{
-    Question.setKysymys_id(Integer.parseInt(request.getParameter("kysymys_id")));
-    Question.setKysymys(request.getParameter("kysymys"));
+	
+    //Question.setKysymys_id(Integer.parseInt(request.getParameter("kysymys_id")));
+    //Question.setKysymys(request.getParameter("kysymys"));
 
-  }
+  
 
 public Update() {
     super();
@@ -32,17 +41,31 @@ public Update() {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
+
+	/**
+	 *
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		ArrayList<Question> list = null;
+		Question f = null;
+//		ArrayList<Question> list = null;
+		int id = Integer.parseInt(request.getParameter("id"));
 		if (dao.getConnection()) {
-			list = dao.updateQuestion();
+//			//list = dao.updateQuestion(null);
+			f = dao.readQuestion(id);
+			
+			ArrayList<String> f = new ArrayList<String>();
+			if (dao.getConnection()) {
+				candi = dao.readCandidate(id);
+				f.add(candi.getEtunimi());
 		} else {
 			System.out.println("No connection to database");
 		}
-		request.setAttribute("questionlist", list);
-
-		RequestDispatcher rd = request.getRequestDispatcher("/jsp/updatequestion.jsp");
-		rd.forward(request, response);
+		
+		
+		request.setAttribute("question", f);
+		response.sendRedirect("/jsp/updatequestion.jsp");
+		//RequestDispatcher rd = request.getRequestDispatcher("/jsp/updatequestion.jsp");
+		//rd.forward(request, response);
 	}
 }
